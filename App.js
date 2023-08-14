94ce3ff2500339420be0e0161f47e3ab1a30abf4
enigma-pasti-fadli <fadli.vendor.pasti@pegadaian.co.id>
2023-08-14 13:38:42 +0700

    intro text and view component on file app.js

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        padding: 20,
-      }}
-    >
-      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
-      <View style={{ backgroundColor: "red", flex: 0.5 }} />
+    <View style={{padding:20}}>      
       <Text>Hello World!</Text>
+      <Text>Hello Enigmanians</Text>
+      <View style={{ width: 100, height: 100, backgroundColor: '#00a8f3' }}>
+        <Text>Square - 1</Text>
+      </View>
+      <View style={{ width: 150, height: 100, backgroundColor: '#ff6400' }}>
+      <Text>Square - 2</Text>
+      </View>
+      <View style={{ width: 200, height: 100, backgroundColor: '#5b6a87' }}>
+      <Text>Square - 3</Text>
+      </View>
+      <StatusBar style="auto" />  
     </View>
   );
 }
+// View Component
+// The most fundamental component for building a UI, View is a container that supports 
+// layout with flexbox, style, some touch handling, and accessibility controls. View maps
+//  directly to the native view equivalent on whatever platform React Native is running on,
+//   whether that is a UIView, <div>, android.view, etc.
+
